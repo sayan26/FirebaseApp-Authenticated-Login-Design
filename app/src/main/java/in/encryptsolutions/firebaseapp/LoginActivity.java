32342b98dc77334127a37bed35cadfcb6b7ad9ad
Sayan Bhattacharya <sayanbhattacharya06@gmail.com>
2018-06-01 21:06:26 +0530

    Initial commit

---
+++ app/src/main/java/in/encryptsolutions/firebaseapp/LoginActivity.java
@@ -0,0 +1,35 @@
+package in.encryptsolutions.firebaseapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    Button loginButton;
+    ProgressBar loginProgress;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginButton = findViewById(R.id.login_button);
+        loginProgress = findViewById(R.id.login_progress);
+        loginProgress.setVisibility(View.INVISIBLE);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                loginProgress.setVisibility(View.VISIBLE);
+                loginButton.setVisibility(View.INVISIBLE);
+
+            }
+        });
+    }
+}
